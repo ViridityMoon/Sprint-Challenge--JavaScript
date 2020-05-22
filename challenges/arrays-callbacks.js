4a6ba6809af311bca4c7344afcf7334d6e65c661
ViridityMoon <viriditymoon@gmail.com>
2020-05-22 13:25:09 -0400

    touch ups after 1-1

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -24,7 +24,7 @@ The zoos want to display both the scientific name and the animal name in front o
 const displayNames = [];
 
 zooAnimals.forEach(function(data){
-  displayNames.push({Name: data.animal_name, Scientific: data.scientific_name});
+  displayNames.push(`{Name: ${data.animal_name}, Scientific: ${data.scientific_name}}`);
 })
 
 console.log(displayNames);
@@ -46,9 +46,9 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-// const lowPopulationAnimals = zooAnimals.filter((data) => {
-//   return data.population > 5
-// })
+const lowPopulationAnimals = zooAnimals.filter((data) => {
+  return data.population > 5
+})
 // console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -59,7 +59,7 @@ The zoos need to know their total animal population across the United States. Fi
 let populationTotal = zooAnimals.reduce((total, data) => {
   return total += data.population;
 }, 0)
-console.log(populationTotal);
+// console.log(populationTotal);
 
 
 // ==== Callbacks ====  
@@ -70,29 +70,29 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-// function consume(a, b, cb){
-//   return cb(a, b);
-// }
+function consume(a, b, cb){
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-// function add(a, b){
-//   return a + b;
-// }
-// function multiply(a, b){
-//   return a*b;
-// }
-// function greeting(firstName, lastName){
-//   return `Hello ${firstName} ${lastName}, nice to meet you!`
-// }
+function add(a, b){
+  return a + b;
+}
+function multiply(a, b){
+  return a*b;
+}
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -3,33 +3,33 @@
 /* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
 
 
-// const external = "I'm outside the function";
+const external = "I'm outside the function";
 
-// function myFunction() {
-//   console.log(external);
-//   const internal = "Hello! I'm inside myFunction!";
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
 
-//   function nestedFunction() {
-//     console.log(internal);
-//   };
-//   nestedFunction();
-// }
-// myFunction();
+  function nestedFunction() {
+    console.log(internal);
+  };
+  nestedFunction();
+}
+myFunction();
 
 // Explanation: 
-  // The nestedFunction is nested within myFunction and therefore can access the variable within myFunction as well, this is refers to closure.
+  // The nestedFunction is nested within myFunction and therefore can access the variable within myFunction as well, this refers to closure.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
-// function sumation(num1){
-//   return num1 * (num1 + 1) / 2;
-// }
+function sumation(num1){
+  return num1 * (num1 + 1) / 2;
+}
 
-// console.log(sumation(1));
-// console.log(sumation(2));
-// console.log(sumation(3));
-// console.log(sumation(4));  
-// console.log(sumation(5));
-// console.log(sumation(6));
+console.log(sumation(1));
+console.log(sumation(2));
+console.log(sumation(3));
+console.log(sumation(4));  
+console.log(sumation(5));
+console.log(sumation(6));
